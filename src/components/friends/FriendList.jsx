0ae7143e8f15670eae17879024017f445c49193e
Maribelaya <shandermarina7@gmail.com>
2024-03-23 23:57:54 +0100

    part

--- src/components/friends/FriendList.jsx
+++ src/components/friends/FriendList.jsx
@@ -1,21 +1,23 @@
 import FriendListItem from "./FriendListItem";
 import css from "./FriendList.module.css";
 
-const FriendList = (friends) => {
+const FriendList = ({ friends }) => {
   return (
-    <ul className={css["list-item"]}>
-      {friends.map(({ friend }) => {
-        return (
-          <li key={friend.id}>
-            <FriendListItem
-              avatar={friend.avatar}
-              name={friend.name}
-              isOnline={friend.isOnline}
-            />
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul className={css["list-item"]}>
+        {friends.map((friend) => {
+          return (
+            <li key={friend.id}>
+              <FriendListItem
+                avatar={friend.avatar}
+                name={friend.name}
+                isOnline={friend.isOnline}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 export default FriendList;
--- src/components/friends/FriendListItem.jsx
+++ src/components/friends/FriendListItem.jsx
@@ -2,16 +2,16 @@ import css from "./FriendList.module.css";
 
 const FriendListItem = ({ id, avatar, name, isOnline }) => {
   return (
-    <li className={css.item} key={id}>
+    <div className={css.item} key={id}>
       <img className={css.photo} src={avatar} alt="Avatar" width="48" />
-      <p className={name}>{name}</p>
+      <p className={css.name}>{name}</p>
 
       {isOnline ? (
         <p className={css.online}>Online</p>
       ) : (
         <p className={css.offline}>Offline</p>
       )}
-    </li>
+    </div>
   );
 };
 
